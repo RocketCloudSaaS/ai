5de5ef57aff76049bea9d2ef510074128092fce9
Enric Tobella <enric.tobella@dixmit.com>
2026-05-18 09:55:29 +0200

    [IMP] ai_tool: Pre-commit stuff

--- ai_tool/models/ai_tool.py
+++ ai_tool/models/ai_tool.py
@@ -15,7 +15,6 @@ except ImportError:
 
 
 class AiTool(models.Model):
-
     _name = "ai.tool"
     _description = "AI Tool"
 
@@ -89,6 +88,7 @@ class AiTool(models.Model):
             )
         elif record._name != self.model_id.model:
             raise ValueError(
-                f"Record model {record._name} does not match tool model {self.model_id.model}"
+                f"Record model {record._name} does not match tool "
+                f"model {self.model_id.model}"
             )
         return getattr(record, self.function_name)(*args, **kwargs) or {}
